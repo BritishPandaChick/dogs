e6b6bcf02fad5e5c52684e2f679f32549575465e
britishpandachick <sbartley1080@gmail.com>
2023-06-22 20:40:25 -0400

    Update WordPress to 6.2.2. Update plugins to work with latest WordPress version

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"=Lc08 l,M,3:[H}ZI+VA TO`FT/puA)HS tk*[xhuEL<77DTj9cPOFh3cplBT)S^";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1270;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"=Lc08 l,M,3:[H}ZI+VA TO`FT/puA)HS tk*[xhuEL<77DTj9cPOFh3cplBT)S^";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1270;}
